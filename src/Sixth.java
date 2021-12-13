250ca3fdccdb8c5aa3fce0555402f831373c8cf3
Karl Öqvist <karloq@student.chalmers.se>
2021-12-13 12:41:57 +0100

    Changes to day 6 solution

--- src/Sixth.java
+++ src/Sixth.java
@@ -5,8 +5,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sixth {
-    public static ArrayList<Integer> fishes = new ArrayList<>();
-    public static ArrayList<Long> fishes_temp = new ArrayList<>((
+    private static ArrayList<Long> fishes = new ArrayList<>((
             Arrays.asList(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L)));
 
     public static void main(String[] args) {
@@ -14,32 +13,32 @@ public class Sixth {
         System.out.println(simulate(256));
     }
 
-    public static Long simulate(int days) {
+    private static Long simulate(int days) {
         for (int i = 0; i < days; i++) {
             ArrayList<Long> fishes_temp = new ArrayList<>((
                     Arrays.asList(0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L, 0L)));
-            for (int dayGroup = 0; dayGroup < Sixth.fishes_temp.size(); dayGroup++) {
+            for (int dayGroup = 0; dayGroup < Sixth.fishes.size(); dayGroup++) {
                 if (dayGroup == 0) {
-                    if (Sixth.fishes_temp.get(dayGroup) > 0){
-                        fishes_temp.set(6, Sixth.fishes_temp.get(dayGroup));
-                        fishes_temp.set(8, fishes_temp.get(8) + Sixth.fishes_temp.get(dayGroup));
+                    if (Sixth.fishes.get(dayGroup) > 0){
+                        fishes_temp.set(6, Sixth.fishes.get(dayGroup));
+                        fishes_temp.set(8, fishes_temp.get(8) + Sixth.fishes.get(dayGroup));
                     }
                 } else {
-                    fishes_temp.set(dayGroup - 1, fishes_temp.get(dayGroup - 1) + Sixth.fishes_temp.get(dayGroup));
+                    fishes_temp.set(dayGroup - 1, fishes_temp.get(dayGroup - 1) + Sixth.fishes.get(dayGroup));
                 }
             }
             for (int j = 0; j < fishes_temp.size(); j++) {
-                Sixth.fishes_temp.set(j, fishes_temp.get(j));
+                Sixth.fishes.set(j, fishes_temp.get(j));
             }
         }
         Long sum = 0L;
-        for (Long total: fishes_temp){
+        for (Long total: fishes){
             sum = sum + total;
         }
         return sum;
     }
 
-    public static void readFile() {
+    private static void readFile() {
         File txt = new File("resources/6.txt");
         Scanner scan = null;
         try {
@@ -51,9 +50,8 @@ public class Sixth {
             String[] split = scan.nextLine().split(",");
             for (String value : split) {
                 int data = Integer.parseInt(value);
-                fishes.add(data);
-                Long current = fishes_temp.get(data);
-                fishes_temp.set(data, current + 1);
+                Long current = fishes.get(data);
+                fishes.set(data, current + 1);
             }
         }
     }
