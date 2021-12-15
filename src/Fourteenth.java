573179041abce757bc6f1a1dd6d8550ef9997962
Karl Öqvist <karloq@student.chalmers.se>
2021-12-15 12:32:15 +0100

    Add Day 14 part 2

--- src/Fourteenth.java
+++ src/Fourteenth.java
@@ -7,90 +7,96 @@ import java.util.*;
 
 public class Fourteenth {
 
-    private static ArrayList<Character> polymer = new ArrayList<>();
-    private static HashMap<Pair<Character, Character>,Character> recipeMap = new HashMap<>();
+    private static Map<String, Long> occurances = new HashMap<>();
+    private static Map<String, Long> occurances_temp = new HashMap<>();
+    private static Map<String, String> recipes = new HashMap<>();
+    private static Map<Character, Long> letter_occurances = new HashMap<>();
+    private static String polymer;
 
     public static void main(String[] args) {
         readFile();
+        initPolymer();
+        //System.out.println(occurances.entrySet());
         step(10);
-        System.out.println("Difference for task 1: " + countDifference());
+        System.out.println("Difference after 10 steps: " + count());
+        step(30);
+        System.out.println("Difference after 40 steps: " + count());
     }
 
-    private static int countDifference() {
-        ArrayList<Character> charArray = new ArrayList<>();
-        for (Character letter : polymer) {
-            if (!charArray.contains(letter)) {
-                charArray.add(letter);
-            }
+    private static void initPolymer() {
+        for (int i = 0; i < polymer.length() - 1; i++) {
+            String ab = polymer.substring(i, i + 2);
+            occurances.replace(ab, occurances.get(ab) + 1L);
         }
-        int[] occurancesArray = new int[charArray.size()];
-        for (Character letter : polymer) {
-            int index = charArray.indexOf(letter);
-            occurancesArray[index]++;
+        for (int i = 0; i < polymer.length(); i++) {
+            Character a = polymer.charAt(i);
+            letter_occurances.replace(a, letter_occurances.get(a) + 1L);
         }
-        Arrays.sort(occurancesArray);
-        return (occurancesArray[occurancesArray.length-1]-occurancesArray[0]);
     }
 
     private static void step(int steps) {
-        for (int step = 0; step < steps; step++) {
-            int length = polymer.size();
-            for (int i = 0; i < ((length*2)-2); i++) {
-                Character a = polymer.get(i);
-                Character b = polymer.get(i + 1);
-                polymer.add(i+1,recipeMap.get(new Pair<>(a,b)));
-                i++;
+        for (int i = 0; i < steps; i++) {
+            for (String pair : occurances.keySet()) {
+                Long occurance = occurances.get(pair);
+                String pair1 = pair.toCharArray()[0] + recipes.get(pair);
+                String pair2 = recipes.get(pair) + pair.toCharArray()[1];
+                occurances_temp.replace(pair1, occurances_temp.get(pair1) + occurance);
+                occurances_temp.replace(pair2, occurances_temp.get(pair2) + occurance);
+                Character letter = recipes.get(pair).charAt(0);
+                letter_occurances.replace(letter, letter_occurances.get(letter) + occurance);
             }
+            copyFromTemp();
         }
     }
 
+    private static Long count() {
+        Long max = 0L;
+        Long min = Long.MAX_VALUE;
+        for (Character letter : letter_occurances.keySet()) {
+            Long letter_occurance = letter_occurances.get(letter);
+            if (letter_occurance > 0) {
+                if (letter_occurance > max) {
+                    max = letter_occurance;
+                }
+                if (letter_occurance < min) {
+                    min = letter_occurance;
+                }
+            }
+        }
+        return max - min;
+    }
+
+    private static void copyFromTemp() {
+        for (String key : occurances_temp.keySet()) {
+            occurances.replace(key, occurances_temp.get(key));
+            occurances_temp.replace(key, 0L);
+        }
+    }
+
+
     private static void readFile() {
-        File txt = new File("resources/14_test.txt");
+        File txt = new File("resources/14.txt");
         Scanner scan = null;
         try {
             scan = new Scanner(txt);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        char[] out = scan.nextLine().toCharArray();
-        for (char letter : out) {
-            polymer.add(letter);
-        }
+        polymer = scan.nextLine();
         //remove blank line
         scan.nextLine();
         while (scan.hasNextLine()) {
             String[] split = scan.nextLine().split(" -> ");
-            char[] ab = split[0].toCharArray();
-            char[] c = split[1].toCharArray();
-            recipeMap.put(new Pair<>(ab[0],ab[1]),c[0]);
+            String ab = split[0];
+            Character a = ab.toCharArray()[0];
+            Character b = ab.toCharArray()[1];
+            String c = split[1];
+            recipes.put(ab, c);
+            occurances.put(ab, 0L);
+            occurances_temp.put(ab, 0L);
+            letter_occurances.put(a, 0L);
+            letter_occurances.put(b, 0L);
         }
     }
 
-    public static class PolyRecipe {
-        char mono_1;
-        char mono_2;
-        char result;
-
-        public PolyRecipe(char a, char b, char c) {
-            mono_1 = a;
-            mono_2 = b;
-            result = c;
-        }
-
-        @Override
-        public String toString() {
-            StringBuilder out = new StringBuilder();
-            out.append(mono_1);
-            out.append(mono_2);
-            out.append(" -> ");
-            out.append(result);
-            return out.toString();
-        }
-
-        public Character check(char a, char b) {
-            if ((this.mono_1 == a) && (this.mono_2 == b)) {
-                return result;
-            } else return null;
-        }
-    }
 }
